37ff213871a53a733d2db272931b9d0b8db64bc4
sd2011 <30780794+sd2011@users.noreply.github.com>
2024-07-29 12:36:40 +0300

    handle cors

--- server/handler.js
+++ server/handler.js
@@ -30,7 +30,7 @@ module.exports.randomVideo = async (event) => {
         resolve({
           statusCode: 200,
           headers: {
-      'Access-Control-Allow-Origin': process.env.CLIENT_DOMAIN, // <-- Add your specific origin here
+      'Access-Control-Allow-Origin': "*", // <-- Add your specific origin here
       'Access-Control-Allow-Credentials': true,
     },
           body: JSON.stringify(
