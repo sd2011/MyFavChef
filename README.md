2c73214e1212c734f39fab650da82ab5d04c6418
sd2011 <30780794+sd2011@users.noreply.github.com>
2024-08-02 19:27:10 +0300

    add webhook to insert new videos added to channel to the DB

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MyFavChef
 A website to show a random recipe from my favorite chef -> https://myfavchef.netlify.app/
+
+All videos data is stored at a MySQL RDS on AWS.
+Using lambadas to get and insert videos.
+Subscribed to PubSubHubbub to get notify each time a new video was added to the channel
+and use a webhook to insert new video data to the DB -> https://pubsubhubbub.appspot.com/subscribe 
--- server/handler.js
+++ server/handler.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const xml2js = require('xml2js');
 
 
 const dbConfig = {
@@ -8,12 +9,14 @@ const dbConfig = {
   database: process.env.DB_NAME
 };
 
+const tableName = 'Videos';
+
 module.exports.randomVideo = async (event) => { 
   return new Promise((resolve, reject) => {
     const connection = mysql.createConnection(dbConfig);
      connection.query(`
       SELECT videoId
-      FROM Videos
+      FROM ${tableName}
       ORDER BY RAND()
       LIMIT 1
     `, (error, results) => {
@@ -46,3 +49,56 @@ module.exports.randomVideo = async (event) => {
   });
 
 };
+
+module.exports.youtubeWebhook = async (event) => {
+  return new Promise(async (resolve, reject) => {
+    const headers = event.headers;
+    const body = event.body;
+    if (headers['X-Hub-Signature']) {
+      const signature = headers['X-Hub-Signature'].split('=')[1];
+      if (!verifySignature(body, signature, process.env.HUB_SECRET)) {
+        console.error('Invalid signature');
+        return {
+          statusCode: 403,
+          body: 'Forbidden'
+        };
+      }
+    }
+
+    if (event.queryStringParameters && event.queryStringParameters['hub.challenge']) {
+      console.error('Verification request');
+      return {
+        statusCode: 200,
+        body: event.queryStringParameters['hub.challenge']
+      };
+    }
+    const parser = new xml2js.Parser();
+    const feed = await parser.parseStringPromise(body);
+    const connection = await mysql.createConnection(dbConfig);
+
+    const values = feed.feed.entry.map((entry) => [entry['yt:videoId'][0], entry['title'][0]]);
+    const insertQuery = `INSERT INTO ${tableName} (videoId, title) VALUES ?`;
+
+    connection.query(insertQuery, [values], (error, results) => {
+    if (error) {
+      console.error('Error inserting data:', error);
+      reject({
+          message: 'Error inserting data', error })
+    }
+    console.error('Insert results:', results);
+    resolve({
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Data inserted successfully', results })
+    });
+    });
+  });
+  
+};
+
+const verifySignature = (payload, signature, secret) => {
+const crypto = require('crypto');
+
+  const mac = crypto.createHmac('sha1', secret).update(payload).digest('hex');
+  console.error('MAC:', mac);
+  return crypto.timingSafeEqual(Buffer.from(mac, 'hex'), Buffer.from(signature, 'hex'));
+}
